73695c8768d70ddbda0a04062edc1fbc2e2c75d7
dshahmir <shahmirx5@gmail.com>
2023-05-22 19:22:14 +0500

    Dynamic

---
+++ src/api/fitness/controllers/fitness.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fitness controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fitness.fitness');
